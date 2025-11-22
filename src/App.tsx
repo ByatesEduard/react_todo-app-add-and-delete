dc0f08a54cda892c612bca3356237d1d053716e6
ByatesEduard <edikbatec1@gmail.com>
2025-11-22 16:49:45 +0200

    add soltuion

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,128 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { getTodos, USER_ID } from './api/todos';
+import { Todo } from './types/Todo';
+import { FilterEnum, Footer } from './components/Footer/footer';
+import { Error } from './components/Error/errorMessage';
+import { Header } from './components/Header/header';
+import { TodoItem } from './components/TodoItem/todoItem';
+import * as todoService from './api/todos';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [filter, setFilter] = useState<FilterEnum>(FilterEnum.all);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [filteredTodos, setFilteredTodos] = useState<Todo[] | null>(todos);
+  const [isAdding, setIsAdding] = useState(false);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    async function fetchTodos() {
+      try {
+        const todosFromServer = await getTodos();
+
+        setTodos(todosFromServer);
+      } catch (error) {
+        setErrorMessage('Unable to load todos');
+        setTimeout(() => setErrorMessage(null), 3000);
+      }
+    }
+
+    fetchTodos();
+  }, []);
+
+  useEffect(() => {
+    if (filter && todos) {
+      switch (filter) {
+        case FilterEnum.all:
+          setFilteredTodos(todos);
+          break;
+        case FilterEnum.active:
+          setFilteredTodos(todos.filter(todo => !todo.completed));
+          break;
+        case FilterEnum.completed:
+          setFilteredTodos(todos.filter(todo => todo.completed));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    }
+  }, [filter, todos]);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
-  return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+  function toggleTodoCompleted(todoId: number) {
+    setTodos(prevTodos => {
+      if (!prevTodos) {
+        return prevTodos;
+      }
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      setIsLoaded(true);
+      setTimeout(() => {
+        setIsLoaded(false);
+      }, 100);
+
+      return prevTodos.map(todo =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
+      );
+    });
+  }
+
+  function handleDeleteTodo(todoId: number) {
+    setIsAdding(true);
+    todoService
+      .deleteTodo(todoId)
+      .then(() => {
+        setTodos(prev => prev.filter(todo => todo.id !== todoId));
+      })
+      .catch(() => {
+        setErrorMessage('Unable to delete todo');
+      })
+      .finally(() => {
+        setIsAdding(false);
+      });
+  }
+
+  return (
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          setTodos={setTodos}
+          setErrorMessage={setErrorMessage}
+          setTempTodo={setTempTodo}
+          userId={USER_ID}
+          setIsAdding={setIsAdding}
+          setTitle={setTitle}
+          title={title}
+          isAdding={isAdding}
+        />
+
+        <section className="todoapp__main" data-cy="TodoList">
+          {filteredTodos?.map((todo: Todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              isLoaded={isLoaded}
+              deleteTodo={handleDeleteTodo}
+              toggleTodoCompleted={toggleTodoCompleted}
+            />
+          ))}
+        </section>
+
+        {todos && todos.length > 0 && (
+          <Footer todos={todos} filter={filter} setFilter={setFilter} />
+        )}
+      </div>
+
+      <Error errorMsg={errorMessage} />
+    </div>
   );
 };
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,94 @@
+import React, { useRef } from 'react';
+import { Todo } from '../../types/Todo';
+import * as todoService from '../../api/todos';
+
+type Props = {
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  setErrorMessage: React.Dispatch<React.SetStateAction<string | null>>;
+  setTempTodo: React.Dispatch<React.SetStateAction<Todo | null>>;
+  userId: number;
+  setIsAdding: React.Dispatch<React.SetStateAction<boolean>>;
+  title: string;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+  isAdding: boolean;
+};
+
+export const Header: React.FC<Props> = ({
+  setTodos,
+  setErrorMessage,
+  setTempTodo,
+  userId,
+  setIsAdding,
+  title,
+  setTitle,
+  isAdding,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleAddTodo(e: React.FormEvent) {
+    e.preventDefault();
+
+    const trimmed = title.trim();
+
+    if (!trimmed) {
+      setErrorMessage('Title should not be empty');
+
+      return;
+    }
+
+    const tempTodo: Todo = {
+      id: 0,
+      title: trimmed,
+      completed: false,
+      userId,
+    };
+
+    setTempTodo(tempTodo);
+    setIsAdding(true);
+
+    todoService
+      .addTodo({
+        title: trimmed,
+        userId,
+        completed: false,
+      })
+      .then((newTodo: Todo) => {
+        setTodos(prev => [...prev, newTodo]);
+        setTitle('');
+      })
+      .catch(() => {
+        setErrorMessage('Unable to add todo');
+      })
+      .finally(() => {
+        setIsAdding(false);
+        setTempTodo(null);
+        inputRef.current?.focus();
+      });
+  }
+
+  return (
+    <header className="todoapp__header">
+      <form onSubmit={handleAddTodo}>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          ref={inputRef}
+          disabled={isAdding}
+        />
+      </form>
+
+      {/* Кнопка для toggle-all, для a11y */}
+      <button
+        type="button"
+        className="todoapp__toggle-all active"
+        data-cy="ToggleAllButton"
+        aria-label="Toggle all todos"
+        disabled={isAdding}
+      />
+    </header>
+  );
+};
